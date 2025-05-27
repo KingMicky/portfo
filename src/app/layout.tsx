e0fe68946ae90f76ecce7083b920c42b5d8d369a
KingMicky <dikachi.ogalabu@gmail.com>
2025-05-27 09:20:56 +0100

    Add @vercel/speed-insights dependency and import in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Tenor_Sans, Cardo } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 import Wrapper from "@/components/Wrapper";
 import Navigation from "@/components/Navigation";
